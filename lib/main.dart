f23e00cdd6b599731f1e80813443890bc8ee9457
abualmun <muntherhafiz474@gmail.com>
2025-07-15 13:36:32 +0300

    munther map merged

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:void_path/custom_widgets/openstreetmap_screen.dart';
+import 'package:public_transportation/custom_widgets/openstreetmap_screen.dart';
 
 void main() {
   runApp(const MyApp());
